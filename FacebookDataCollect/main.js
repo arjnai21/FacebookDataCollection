feb1b45edebd2eaabd4ac6de2b7a71fd628bd570
ArjunNair03 <arjun@nairsnet.com>
2019-01-16 10:09:01 -0500

    set up skeleton for code

--- FacebookDataCollect/main.js
+++ FacebookDataCollect/main.js
@@ -1,28 +1,46 @@
 alert(window.location.href);
 if(window.location.href === "https://twitter.com/RudyGiuliani/status/1084428955384496128"){
+    var likes = getNumLikes();
+    var shares = getNumShares();
+    var replies = getNumReplies();
 
 }
 
 function getNumLikes(){
-
+    var x = document.getElementsByClassName("request-retweeted-popup");
+    alert(x);
 }
 
 function getNumShares(){
-
+    var x = document.getElementsByClassName("request-favorited-popup");
+    alert(x);
 }
 
 function getNumReplies() {
-    
+
+    var x = document.getElementsByClassName("ProfileTweet-actionCount");
+    alert(x[0].getAttribute("data-tweet-stat-count"));
+
 }
 
-function getUsersList(){
-    
+function getUserRetweetsList(){
+    var userList = document.getElementsByClassName("account-group js-user-profile-link");
+    return userList;
 }
 
-function searchUserProfile(){
+function searchUserProfile(user, str){
 
 }
 
 function iterateUsersList() {
+    var userList = getUserRetweetsList();
+    var userHitDict = {};
+    for (let i = 0; i < userList; i++) {
+        var hits = searchUserProfile(userList[i], "CNN");
+        userHitDict[userList[i]] = hits;
+    }
+
+    return userHitDict;
+
 
 }
