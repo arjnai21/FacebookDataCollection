f35ca25cc491406a677d60590cee2918e1506993
ArjunNair03 <arjun@nairsnet.com>
2019-01-16 09:21:47 -0500

    changed to twitter, wrote function names the goal is clear

--- FacebookDataCollect/main.js
+++ FacebookDataCollect/main.js
@@ -1,1 +1,28 @@
 alert(window.location.href);
+if(window.location.href === "https://twitter.com/RudyGiuliani/status/1084428955384496128"){
+
+}
+
+function getNumLikes(){
+
+}
+
+function getNumShares(){
+
+}
+
+function getNumReplies() {
+    
+}
+
+function getUsersList(){
+    
+}
+
+function searchUserProfile(){
+
+}
+
+function iterateUsersList() {
+
+}
