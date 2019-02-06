32cc735e0ef9ae7ac623e72bc23cff04e6095e65
ArjunNair03 <arjun@nairsnet.com>
2019-02-06 09:59:42 -0500

    set mainIteration to run on onload

--- TwitterDataCollect/main.js
+++ TwitterDataCollect/main.js
@@ -1,80 +1,78 @@
-//alert(window.location.href);
-
 
 /* flow -
-main
-iterate userlist
-opennewtab each user
-eac huser search profile
-save in local storage
-
+get repliers for first post and convert into list of nodes
+for each of those nodes open profile and find influenced tweets
+open influenced tweets and get repliers and search their profile
  */
 //main function
 post = "https://twitter.com/PV_Hockey_Assoc/status/1084261852324995072";
+var allNodes = {};
 if(window.location.href === post){ // to do if looking at a post
-    var users = getUserRetweetsList();
-    localStorage.setItem("key", "value")
-    window.open("https://www.google.com")
+    retweetsList = getUserRepliesList(window);
     id = 0;
-    //openNewTab(users[0]);
-}
-else if (window.location.href === "twitter.com"){
-    var d = localStorage.getItem("key")
+    replierNodes = repliesListIntoNodes(getUserRepliesList(window));
+    for (let i = 0; i < replierNodes.length; i++) {
+        let profWindow = window.open(replierNodes[i].link);
+        profWindow.location.href = replierNodes[i].link;
+        profWindow.onload= function (){mainIteration("So", profWindow, replierNodes[i])}
+    }
 }
 
-//searchUserProfile
 
 function repliesListIntoNodes(repliesList){
-    nodesList = [];
+    let nodesList = [];
     for (let i = 0; i < repliesList.length; i++) {
-        var newNode = {};
+        let newNode = {};
         newNode.id = id++;
-        // here we get the full url for the userProfile newNode.link =
+        newNode.link = repliesList[i].getAttribute("href");
         newNode.influenced = false; //set to false for default
         nodesList.push(newNode);
+        allNodes[newNode.id.toString()] = newNode;
     }
     return nodesList
 }
 
-function mainIteration(keyword, profWindow, userNode){
-    var doc = profWindow.document;
-    var tweetsList = doc.getElementsByClassName("js-tweet-text-container");
-    for (var i = 0; i < tweetsList.length; i++){
-        if(tweetsList[i].innerHTML.includes(keyword)){
+function mainIteration(keyword, profWindow, userNode) {
+    let doc = profWindow.document;
+    let tweetsList = doc.getElementsByClassName("js-tweet-text-container");
+    let link;
+    for (let i = 0; i < tweetsList.length; i++) {
+        if (tweetsList[i].innerHTML.toLowerCase().includes(keyword.toLowerCase())) {
             userNode.influenced = true;
-            var link = tweetsList[i].getElementsByClassName("TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")[0].getElementsByTagName("a").getAttribute("href");
-            var postWindow = window.open(link);
-            userNode.children = repliesListIntoNodes(getUserRepliesList(postWindow));
-            for (let j = 0; j < userNode.children; j++) {
-                var theWindow = window.open(userNode.children[i].link);
-                mainIteration(keyword, theWindow, userNode.children[i])
+            let links = tweetsList[i].getElementsByClassName("TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")[0].getElementsByTagName("a")
+            for (let i = 0; i < links.length; i++) {
+                if (!links[i].includes("hashtag")) {
+                    link = links[i];
+                    break;
+                }
             }
-                //for each child open prof window and skrt
+            let postWindow = window.open(link);
+            postWindow.onload = function () {
+                userNode.children = repliesListIntoNodes(getUserRepliesList(postWindow));
+                for (let j = 0; j < userNode.children; j++) {
+                    let theWindow = window.open(userNode.children[i].link);
+                    mainIteration(keyword, theWindow, userNode.children[i])
+                }
+            }
         }
     }
 }
-
-
-
 
 //working
 function getUserRepliesList(theWindow){
-    var doc = theWindow.document;
-    var userList = doc.getElementsByClassName("account-group js-user-profile-link");
-    var repliesList = [];
+    let doc = theWindow.document;
+
+    let userList = doc.getElementsByClassName("account-group js-user-profile-link");
+    let repliesList = [];
     for (let i = 1; i < userList.length; i++) {
-        if(userList[i].tagName !== "DIV"){
+        if (userList[i].tagName !== "DIV") {
             repliesList.push(userList[i]);
         }
-
     }
-
     return repliesList;
-
-
 }
 
-function openNewTab(user){
+/*function openNewTab(user){
     //text = new XMLSerializer().serializeToString(document);
     var link = "https://twitter.com" + user.getAttribute("href");
     //open new tab with that link, search entire dom with innerHTML
@@ -91,11 +89,11 @@ function openNewTab(user){
     }*/
     //var text = document.documentElement.innerHTML;
 
-}
 
+/*
 function iterateUsersList() {
-    var userList = getUserRepliesList();
-    var userHitDict = {};
+    let userList = getUserRepliesList();
+    let userHitDict = {};
     for (let i = 0; i < userList.length; i++) {
         openNewTab(userList[i]);
         userHitDict[userList[i]] = hits;
@@ -105,3 +103,4 @@ function iterateUsersList() {
 
 
 }
+*/
