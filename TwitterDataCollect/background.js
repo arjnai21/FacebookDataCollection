86f4f988e9f13230da58a0105b5ab4118d4f7fe1
arjnai21 <34247946+arjnai21@users.noreply.github.com>
2019-03-11 22:10:01 -0400

    Delete background.js

--- TwitterDataCollect/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if( request.message === "open_new_tab" ) {
-            chrome.tabs.create({"url": request.url});
-        }
-
-    }
-);
-
